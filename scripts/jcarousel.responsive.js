2dc36bdeb6e268d434bd23953072f21c11df3730
felix omewomano obogbare <felixtus4sure@gmail.com>
2016-06-27 07:04:51 +0100

    first official commit

---
+++ scripts/jcarousel.responsive.js
@@ -0,0 +1,106 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var width = jcarousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                jcarousel.jcarousel('items').css('width', width + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+    });
+})(jQuery);
+(function($) {
+    'use strict';
+
+    $.jCarousel.plugin('jcarouselAutoscroll', {
+        _options: {
+            target:    '+=1',
+            interval:  3000,
+            autostart: true
+        },
+        _timer: null,
+        _init: function () {
+            this.onDestroy = $.proxy(function() {
+                this._destroy();
+                this.carousel()
+                    .one('jcarousel:createend', $.proxy(this._create, this));
+            }, this);
+
+            this.onAnimateEnd = $.proxy(this.start, this);
+        },
+        _create: function() {
+            this.carousel()
+                .one('jcarousel:destroy', this.onDestroy);
+
+            if (this.options('autostart')) {
+                this.start();
+            }
+        },
+        _destroy: function() {
+            this.stop();
+            this.carousel()
+                .off('jcarousel:destroy', this.onDestroy);
+        },
+        start: function() {
+            this.stop();
+
+            this.carousel()
+                .one('jcarousel:animateend', this.onAnimateEnd);
+
+            this._timer = setTimeout($.proxy(function() {
+                this.carousel().jcarousel('scroll', this.options('target'));
+            }, this), this.options('interval'));
+
+            return this;
+        },
+        stop: function() {
+            if (this._timer) {
+                this._timer = clearTimeout(this._timer);
+            }
+
+            this.carousel()
+                .off('jcarousel:animateend', this.onAnimateEnd);
+
+            return this;
+        }
+    });
+}(jQuery));
+
+
---
+++ scripts/omano.js
@@ -0,0 +1,35 @@
+$(document).ready
+$('.img1').click(function(){ $('#display').load('images.html #img1') })
+$('.img2').click(function(){ $('#display').load('images.html #img2') })
+$('.img3').click(function(){ $('#display').load('images.html #img3') })
+$('.img4').click(function(){ $('#display').load('images.html #img4') })
+$('.img5').click(function(){ $('#display').load('images.html #img5') })
+$('.img6').click(function(){ $('#display').load('images.html #img6') })
+$('.img7').click(function(){ $('#display').load('images.html #img7') })
+$('.img8').click(function(){ $('#display').load('images.html #img8') })
+$('.img9').click(function(){ $('#display').load('images.html #img2') })
+$('.img10').click(function(){ $('#display').load('images.html #img5') })
+
+$('.img11').click(function(){ $('#display').load('images.html #img11') })
+$('.img12').click(function(){ $('#display').load('images.html #img12') })
+$('.img13').click(function(){ $('#display').load('images.html #img13') })
+$('.img14').click(function(){ $('#display').load('images.html #img14') })
+$('.img15').click(function(){ $('#display').load('images.html #img15') })
+
+$('.img16').click(function(){ $('#display').load('images.html #img11') })
+$('.img17').click(function(){ $('#display').load('images.html #img12') })
+$('.img18').click(function(){ $('#display').load('images.html #img13') })
+$('.img19').click(function(){ $('#display').load('images.html #img14') })
+$('.img20').click(function(){ $('#display').load('images.html #img15') })
+
+$('.stmore').click(function(){ $('.studautohide').show() })
+$('.stless').click(function(){ $('.studautohide').hide() })
+
+$('.enmore').click(function(){ $('.envautohide').show() })
+$('.enless').click(function(){ $('.envautohide').hide() })
+
+$('.lamore').click(function(){ $('.labautohide').show() })
+$('.laless').click(function(){ $('.labautohide').hide() })
+
+$('.detail').click(function(){ $(".detailed-info").show();$(".off-switch").show();$('.to-hide').hide()})
+$('.less-detail').click(function(){ $('.detailed-info').hide(); $(".off-switch").hide(); $('.to-hide').show()})
